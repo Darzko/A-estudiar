bcd586057ff96b4bc23f45d81317140bb794119a
thiago <darzkop@gmail.com>
2023-01-26 04:26:21 -0300

    cosa

---
+++ fundamentos de programacion/eventos.js
@@ -0,0 +1,6 @@
+
+document.getElementById("boton").onclick = function () {
+    console.log("Lograste hacer aparecer la informacion personal!")
+    document.getElementById("informacion").innerHTML = "Programador Front End, con 2 años de experiencia en paginas completas o estaticas. Mi conocimiento incluye un buen manejo de HTML, CSS y Javascript." 
+}
+
